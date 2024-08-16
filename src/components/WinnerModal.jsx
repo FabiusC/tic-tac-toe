8ace23b85964ddd09e49c70b76d4057d089576a9
FabiusC <fabioandrescar@gmail.com>
2024-08-16 17:22:32 -0500

    Fix the string for tie validation

--- src/components/WinnerModal.jsx
+++ src/components/WinnerModal.jsx
@@ -3,7 +3,7 @@ import { Square } from "./Square"
 // eslint-disable-next-line react/prop-types
 export function WinnerModal({winner, resetGame}) {
     if (winner === null) return null
-    const winnerText = winner === '💀' ? "It's a tie" : `The winner is:`
+    const winnerText = winner === '🫱🏽‍🫲🏾' ? "It's a tie" : `The winner is:`
     return (
         <section className="winner" >
             <div className="text">
